b42356f596942d6b2476f1f35188482905b7a4b5
Kamran Bhatti <k.bhatti5384@yahoo.co.uk>
2023-09-02 18:00:36 +0100

    Subject: continue adding new commands to handle for command runner
    Problem:
    Havent finished implementing command runner
    
    Solution:
    continue implementing command runner commands
    
    Note:
    next commit should include handling invalid commands

--- lib/toy_robot/command_runner.ex
+++ lib/toy_robot/command_runner.ex
@@ -4,10 +4,50 @@ defmodule ToyRobot.CommandRunner do
   def run([{:place, placement} | rest]) do
     table = %Table{x_boundary: 4, y_boundary: 4}
     case Simulation.place(table, placement) do
-      {:ok, simulation} -> simulation
+      {:ok, simulation} -> run(rest, simulation)
       {:error, :invalid_placement} -> run(rest)
     end
   end
 
+  def run([_command | rest]), do: run(rest)
   def run([]), do: nil
+
+  def run([:move | rest], simulation) do
+    new_simulation = simulation
+    |> Simulation.move
+    |> case do
+      {:ok, simulation} -> simulation
+      {:error, :at_table_boundary} -> simulation
+    end
+    run(rest, new_simulation)
+  end
+
+  def run([:turn_left | rest], simulation) do
+    {:ok, simulation} = simulation |> Simulation.turn_left
+    run(rest, simulation)
+  end
+
+  def run([:turn_right | rest], simulation) do
+    {:ok, simulation} = simulation |> Simulation.turn_right
+    run(rest, simulation)
+  end
+
+  def run([], simulation), do: simulation
+  
+  def run(
+    [:report | rest],
+    simulation
+  ) do
+    %{
+        x: x,
+        y: y,
+        facing: facing
+    } = Simulation.report(simulation)
+
+    facing = facing |> Atom.to_string |> String.upcase
+    IO.puts "The robot is at (#{x}, #{y}) and is facing #{facing}"
+
+    run(rest, simulation)
+  end
+
 end
--- test/toy_robot/command_runner_test.exs
+++ test/toy_robot/command_runner_test.exs
@@ -3,6 +3,8 @@ defmodule ToyRobot.CommandRunnerTest do
 
   alias ToyRobot.{CommandRunner, Simulation}
 
+  import ExUnit.CaptureIO
+
   test "handles a valid place command" do
     %Simulation{robot: robot} = CommandRunner.run([{:place, %{x: 1, y: 2, facing: :north}}])
 
@@ -27,6 +29,67 @@ defmodule ToyRobot.CommandRunnerTest do
     assert robot.x == 1
     assert robot.y == 2
     assert robot.facing == :north
+  end
+
+  test "handles a place + move command" do
+    %Simulation{robot: robot} = [
+      {:place, %{x: 1, y: 2, facing: :north}},
+      :move
+    ]
+    |> CommandRunner.run()
+
+    assert robot.x == 1
+    assert robot.y == 3
+    assert robot.facing == :north
+  end
+
+  test "handles a place + invalid move command" do
+    %Simulation{robot: robot} = [
+      {:place, %{x: 1, y: 4, facing: :north}},
+      :move
+    ]
+    |> CommandRunner.run()
+
+    assert robot.x == 1
+    assert robot.y == 4
+    assert robot.facing == :north
+  end
+
+  test "handles a place + turn_left command" do
+    %Simulation{robot: robot} = [
+      {:place, %{x: 1, y: 2, facing: :north}},
+      :turn_left
+    ]
+    |> CommandRunner.run()
+
+    assert robot.x == 1
+    assert robot.y == 2
+    assert robot.facing == :west
+  end
+
+  test "handles a place + turn_right command" do
+    %Simulation{robot: robot} = [
+      {:place, %{x: 1, y: 2, facing: :north}},
+      :turn_right
+    ]
+    |> CommandRunner.run()
+
+    assert robot.x == 1
+    assert robot.y == 2
+    assert robot.facing == :east
+  end
+
+  test "handles a place + report command" do
+    commands = [
+      {:place, %{x: 1, y: 2, facing: :north}},
+      :report
+    ]
+
+    output = capture_io fn -> 
+      CommandRunner.run(commands)
+    end
+
+    assert output |> String.trim == "The robot is at (1, 2) and is facing NORTH"
   end
 
 end
