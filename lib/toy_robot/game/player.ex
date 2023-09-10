3b4714e470763b4e2a4c0b0636377a361420b09a
Kamran Bhatti <k.bhatti5384@yahoo.co.uk>
2023-09-10 17:59:38 +0100

    Subject: implement some more GenServer features

--- lib/toy_robot/game/player.ex
+++ lib/toy_robot/game/player.ex
@@ -1,11 +1,42 @@
 defmodule ToyRobot.Game.Player do
   use GenServer
 
-  def init(robot) do
-    {:ok, robot}
+  alias ToyRobot.{Simulation, Table}
+
+  def start_link(robot) do
+    GenServer.start_link(__MODULE__, robot)
   end
 
-  def handle_call(:report, _from, robot) do
-    {:reply, robot, robot}
+  def start(position) do
+    GenServer.start(__MODULE__, position)
+  end
+
+  def init(robot) do
+    simulation = %Simulation{
+        table: %Table{
+            x_boundary: 4,
+            y_boundary: 4
+        },
+        robot: robot
+    }
+
+    {:ok, simulation}
+  end
+
+  def report(player) do
+    GenServer.call(player, :report)
+  end
+
+  def move(player) do
+    GenServer.cast(player, :move)
+  end
+
+  def handle_call(:report, _from, simulation) do
+    {:reply, simulation |> Simulation.report, simulation}
+  end
+
+  def handle_cast(:move, simulation) do
+    {:ok, new_simulation} = simulation |> Simulation.move()
+    {:noreply, new_simulation}
   end
 end
